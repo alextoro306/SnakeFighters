be816605e893b4b75cd7b7ad1bfb03bfc375004c
olaVI <olavi.kurki2004@gmail.com>
2021-05-07 13:40:23 +0300

    update peli.java

--- src/peli.java
+++ src/peli.java
@@ -1,12 +1,18 @@
+import java.util.Random;
 import java.util.Scanner;
+
 public class peli {
-    private static Scanner sc;
     public static void main(String[] args) {
+        Random r = new Random();
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to SnakeFighters! You are on arena 1");
         System.out.println("Type play in any form to start.");
         String Answer = sc.nextLine();
         if(Answer.toUpperCase().equals("PLAY")){
-    }
+            System.out.println("Arvottu vastustajasi:");
+
+    }else{
+            System.out.println("Type play in any form to start");
+        }
 }
 }
